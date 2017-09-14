2c4ff2a9d51947b9a0104bcead1dc20b2bbf6d94
Andrei Chernyshev <andrey.chernishov@gmail.com>
2017-09-14 13:41:46 +0200

    define application path

--- src/main/kotlin/com/restmonkeys/sensors/SensorsApplication.kt
+++ src/main/kotlin/com/restmonkeys/sensors/SensorsApplication.kt
@@ -2,8 +2,10 @@ package com.restmonkeys.sensors
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import javax.ws.rs.ApplicationPath
 
 @SpringBootApplication
+@ApplicationPath("/api/v1")
 class SensorsApplication
 
 fun main(args: Array<String>) {
