41940b4c81039597716f1e93ec80ba354ffa16fd
gimlet2 <andrey.chernishov@gmail.com>
2017-09-18 23:46:57 +0200

    remove jersey annotation

--- src/main/kotlin/com/restmonkeys/sensors/SensorsApplication.kt
+++ src/main/kotlin/com/restmonkeys/sensors/SensorsApplication.kt
@@ -2,10 +2,8 @@ package com.restmonkeys.sensors
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import javax.ws.rs.ApplicationPath
 
 @SpringBootApplication
-@ApplicationPath("/api/v1")
 class SensorsApplication
 
 fun main(args: Array<String>) {
